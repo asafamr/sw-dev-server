b08f39af438c6cada9c1d1245607177f95cf92af
asafamr <asaf.amrami@gmail.com>
2021-11-16 20:29:33 +0200

    message handling

--- src/sw.ts
+++ src/sw.ts
@@ -9,22 +9,23 @@ declare var clients: Clients;
  *
  */
 
-const pages: Record<string, string> = {};
+const pages: Record<string, any> = {};
 sw.addEventListener("fetch", function (event: FetchEvent) {
   const base = self.location.pathname.slice(0,self.location.pathname.lastIndexOf('/'))
   if (!new URL(event.request.url).pathname.startsWith(base+"/dev")) return null;
   if (event.request.method === "POST") {
     return event.respondWith(Promise.resolve().then(async ()=>{
-      pages[event.request.url] = await event.request.text();
+      pages[event.request.url] = await event.request.json();
       return new Response("OK",{status:200, statusText:'OK'});
     }))
   } else if (event.request.method === "GET" && Object.keys(pages).includes(event.request.url)) {
-    return event.respondWith(new Response(pages[event.request.url], {
+    return event.respondWith(new Response(pages[event.request.url].content, {
       status: 200,
       statusText: "OK",
       headers: {
         "Cache-Control": "no-cache, no-store, must-revalidate",
         Pragma: "no-cache",
+        "Content-Type": pages[event.request.url].mime,
         Expires: "0",
       },
     }));
