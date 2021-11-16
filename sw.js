01fc6300bd77c42ee56b577e274ed7288940d625
asafamr <asaf.amrami@gmail.com>
2021-11-16 13:00:21 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,33 @@
+(() => {
+  // src/sw.ts
+  var sw = self;
+  var pages = {};
+  sw.addEventListener("fetch", function(event) {
+    if (!new URL(event.request.url).pathname.startsWith("/dev"))
+      return null;
+    if (event.request.method === "POST") {
+      return event.respondWith(Promise.resolve().then(async () => {
+        pages[event.request.url] = await event.request.text();
+        return new Response("OK", { status: 200, statusText: "OK" });
+      }));
+    } else if (event.request.method === "GET" && Object.keys(pages).includes(event.request.url)) {
+      return event.respondWith(new Response(pages[event.request.url], {
+        status: 200,
+        statusText: "OK",
+        headers: {
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+          Pragma: "no-cache",
+          Expires: "0"
+        }
+      }));
+    }
+  });
+  sw.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+    console.log("Now ready to handle fetches!");
+  });
+  sw.addEventListener("install", function(event) {
+    console.log("Installing new version");
+    event.waitUntil(sw.skipWaiting());
+  });
+})();
